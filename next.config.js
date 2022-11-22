769fb0de7b4b16dd48ecc490e071f64fa099c4e4
konthon <konthon@hotmail.co.th>
2022-11-22 22:40:39 +0700

    chore: add prettier config

--- next.config.js
+++ next.config.js
@@ -5,20 +5,20 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/:path*",
+        source: '/:path*',
         headers: [
           {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "require-corp",
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
           },
           {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin",
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
           },
         ],
       },
-    ];
+    ]
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,68 +1,66 @@
-import { createFFmpeg, fetchFile, FFmpeg } from "@ffmpeg/ffmpeg";
-import { useEffect, useState } from "react";
-import { DropzoneOptions, FileWithPath, useDropzone } from "react-dropzone";
+import { createFFmpeg, fetchFile, FFmpeg } from '@ffmpeg/ffmpeg'
+import { useEffect, useState } from 'react'
+import { DropzoneOptions, FileWithPath, useDropzone } from 'react-dropzone'
 
-const MAX_WIDTH = 480; // 480p
-const MAX_HEIGHT = 270; // 270p
+const MAX_WIDTH = 480 // 480p
 
 export default function Home() {
-  const [resultVideo, setResultVideo] = useState("");
-  const [ffMpeg, setFfMpeg] = useState<FFmpeg>();
-  const [progress, setProgress] = useState<number>();
+  const [resultVideo, setResultVideo] = useState('')
+  const [ffMpeg, setFfMpeg] = useState<FFmpeg>()
+  const [progress, setProgress] = useState<number>()
 
-  const onDrop: DropzoneOptions["onDrop"] = async (
+  const onDrop: DropzoneOptions['onDrop'] = async (
     acceptedFiles: FileWithPath[]
   ) => {
     if (acceptedFiles[0] && ffMpeg) {
-      console.log(acceptedFiles[0].path);
+      console.log(acceptedFiles[0].path)
       if (!ffMpeg.isLoaded()) {
-        await ffMpeg.load();
+        await ffMpeg.load()
       }
-      ffMpeg.FS("writeFile", "input.mp4", await fetchFile(acceptedFiles[0]));
+      ffMpeg.FS('writeFile', 'input.mp4', await fetchFile(acceptedFiles[0]))
       ffMpeg.setProgress(({ ratio }) => {
-        setProgress(Math.abs(ratio * 100));
-      });
+        setProgress(Math.abs(ratio * 100))
+      })
       await ffMpeg.run(
-        "-i",
-        "input.mp4",
-        "-c:v",
-        "libx264",
-        "-vf",
+        '-i',
+        'input.mp4',
+        '-c:v',
+        'libx264',
+        '-vf',
         `scale='min(${MAX_WIDTH},iw)':-1`,
-        // `scale=iw*${targetScale}:ih*${targetScale}`,
-        "-c:a",
-        "aac",
-        "output.mp4"
-      );
-      const outputVideo = ffMpeg.FS("readFile", "output.mp4");
+        '-c:a',
+        'copy',
+        'output.mp4'
+      )
+      const outputVideo = ffMpeg.FS('readFile', 'output.mp4')
       if (outputVideo) {
         setResultVideo(
           URL.createObjectURL(
-            new Blob([outputVideo.buffer], { type: "video/mp4" })
+            new Blob([outputVideo.buffer], { type: 'video/mp4' })
           )
-        );
+        )
       }
     }
-  };
-  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+  }
+  const { getRootProps, getInputProps } = useDropzone({ onDrop })
 
   useEffect(() => {
     const init = () => {
       const initialFFmpeg = createFFmpeg({
         log: true,
-        corePath: "http://localhost:3000/scripts/ffmpeg-core.js",
-      });
-      setFfMpeg(initialFFmpeg);
-    };
-    init();
-  }, []);
+        corePath: 'http://localhost:3000/scripts/ffmpeg-core.js',
+      })
+      setFfMpeg(initialFFmpeg)
+    }
+    init()
+  }, [])
 
   return (
-    <div className="container">
-      <div className="my-3 p-5 border bg-light" {...getRootProps()}>
+    <div className='container'>
+      <div className='my-3 p-5 border bg-light' {...getRootProps()}>
         <input {...getInputProps()} />
         <div>drag n drop or</div>
-        <button className="btn btn-primary">upload</button>
+        <button className='btn btn-primary'>upload</button>
       </div>
       {ffMpeg &&
         ffMpeg.isLoaded() &&
@@ -70,20 +68,20 @@ export default function Home() {
         progress > 0 &&
         progress < 100 && (
           <button
-            className="btn btn-danger"
+            className='btn btn-danger'
             onClick={() => {
-              ffMpeg.exit();
+              ffMpeg.exit()
             }}
           >
             cancel
           </button>
         )}
       {!!progress && (
-        <div className="progress">
+        <div className='progress'>
           <div
-            className="progress-bar progress-bar-striped progress-bar-animated"
-            role="progressbar"
-            aria-label="Basic example"
+            className='progress-bar progress-bar-striped progress-bar-animated'
+            role='progressbar'
+            aria-label='Basic example'
             style={{ width: `${progress}%` }}
             aria-valuenow={progress}
             aria-valuemin={0}
@@ -96,10 +94,10 @@ export default function Home() {
           src={resultVideo}
           controls
           onLoad={() => URL.revokeObjectURL(resultVideo)}
-          className="mt-3"
-          style={{ maxWidth: "100%" }}
+          className='mt-3'
+          style={{ maxWidth: '100%' }}
         />
       )}
     </div>
-  );
+  )
 }
